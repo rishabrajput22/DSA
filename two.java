d10db8e802f7e15181163cdd464be72d3b426e90
rishabrajput22 <rrajput22497@gmail.com>
2021-04-15 17:01:29 +0530

    five

--- two.java
+++ two.java
@@ -2,10 +2,13 @@ public class two {
     public static void main(String[] args) {
         int a = 123;
         System.out.println(a);
+
+        int bil = 1111;
+        System.out.println(bil);
     }
 }
 
 // 1. go on source control
 // 2. add 
 // 3. commit
-// 4 . temp git
+// 4 . temp  and how to do change in existing file? simple do the change then repeat step 1-4
