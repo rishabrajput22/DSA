ffcb4cc68de0a2ab9639e9799ad50093be9309dd
rishabrajput22 <rrajput22497@gmail.com>
2021-04-15 16:50:34 +0530

    two

---
+++ one.java
@@ -0,0 +1,14 @@
+public interface one {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+    }
+}
+
+// ye sb paste kkre ap ok isko kya karu
+// copy kre first line apni email dale
+// utwaiot
+// wai
+// user name dalsa h 9XbHSnvtdmBiUA6 bhai kya karu ?
+// kaha dala h?
+// acha chl rha tha  
